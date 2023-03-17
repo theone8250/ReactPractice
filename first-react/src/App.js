b90706a118eb1687b3ce4eaac0d957711fd48f24
陳彥嘉 Godfrey <godfreychen@systex.com>
2023-03-17 09:11:32 +0800

    add productlist&img

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Checkout from "./Checkout";
+import ProductDetail from "./ProductDetail";
+import ProductList from "./ProductList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Link to="/product_detail">產品資料</Link>
+      <Link to="/checkout">購物車</Link> */}
+      <BrowserRouter>
+        <Routes>
+          {/* "/"代表首頁   */}
+          <Route path="/" element={<ProductList />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/product_detail" element={<ProductDetail />}></Route>
+          {/* path="*"代表上述網頁都找不到會去的頁面   */}
+
+          <Route path="*" element={<h2>找不到網頁</h2>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ first-react/src/ProductList.js
@@ -0,0 +1,68 @@
+import Link from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+export default function ProductList() {
+  let productList = [
+    {
+      id: 1,
+      name: "蘋果",
+      price: 5,
+      image: "apple.jpg",
+      description: "新鮮蘋果50克，一日一蘋果，醫生遠離我",
+    },
+    {
+      id: 2,
+      name: "橙",
+      price: 3,
+      image: "orange.jpg",
+      description: "新鮮橙50克，又甜又好吃",
+    },
+    {
+      id: 3,
+      name: "芒果",
+      price: 4,
+      image: "mango.jpg",
+      description: "新鮮芒500克，宜做甜品",
+    },
+    {
+      id: 4,
+      name: "西瓜",
+      price: 20,
+      image: "watermelon.jpg",
+      description: "新鮮西瓜2公斤，夏季必備",
+    },
+    {
+      id: 5,
+      name: "藍梅",
+      price: 10,
+      image: "blueberry.jpg",
+      description: "新鮮藍梅50克，補眼之寶",
+    },
+    {
+      id: 6,
+      name: "白蘿蔔",
+      price: 5,
+      image: "white-carrot.jpg",
+      description: "新鮮白蘿蔔1公斤，宜煲湯",
+    },
+  ];
+  return (
+    <div>
+      <h1 style={{ backgroundColor: "orange" }}>請選擇要買的東西</h1>
+      <div>
+        {productList.map((product) => (
+          <div key={product.id}>
+            {product.name}
+            <br />
+            {product.price}
+            <br />
+            <img src={process.env.PUBLIC_URL + "/img/" + product.image} />
+            <br />
+            {product.description}
+            <br />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
