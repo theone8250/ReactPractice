b895b76947bb307cb38588f9805fdd1503c73c4f
陳彥嘉 Godfrey <godfreychen@systex.com>
2023-03-22 20:33:14 +0800

    practice useState,useEffect

--- first-react/src/ProductList.js
+++ first-react/src/ProductList.js
@@ -3,50 +3,15 @@ import React, { useState, useEffect } from "react";
 import styles from "./ProductList.module.css";
 import Title from "./Title";
 export default function ProductList() {
-  let productList = [
-    {
-      id: 1,
-      name: "蘋果",
-      price: 5,
-      image: "apple.jpg",
-      description: "新鮮蘋果50克，一日一蘋果，醫生遠離我",
-    },
-    {
-      id: 2,
-      name: "橙",
-      price: 3,
-      image: "orange.jpg",
-      description: "新鮮橙50克，又甜又好吃",
-    },
-    {
-      id: 3,
-      name: "芒果",
-      price: 4,
-      image: "mango.jpg",
-      description: "新鮮芒500克，宜做甜品",
-    },
-    {
-      id: 4,
-      name: "西瓜",
-      price: 20,
-      image: "watermelon.jpg",
-      description: "新鮮西瓜2公斤，夏季必備",
-    },
-    {
-      id: 5,
-      name: "藍梅",
-      price: 10,
-      image: "blueberry.jpg",
-      description: "新鮮藍梅50克，補眼之寶",
-    },
-    {
-      id: 6,
-      name: "白蘿蔔",
-      price: 5,
-      image: "white-carrot.jpg",
-      description: "新鮮白蘿蔔1公斤，宜煲湯",
-    },
-  ];
+  let [productList, setProductList] = useState([]);
+  useEffect(() => {
+    //1 : 無第二個參數 : component每次render都會觸發
+    //2 : Dependency Array是空array時 : 只會在第一次網頁render時會觸發
+    //3 : Dependency Array是有變數時 : 第一次網頁render時 + 指定的變數改變 會觸發
+    fetch("https://hoyinleung.github.io/demoapi/react-basic-product.json")
+      .then((response) => response.json())
+      .then((data) => setProductList(data));
+  }, []); // <==  Dependency Array
   return (
     <div>
       <Title mainTitle="請選擇要買的東西" />
